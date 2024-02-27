fae490474d91d42dc69b9cebe5e9bd5c418761ef
Micael Macedo <micaelm2009@hotmail.com>
2024-02-26 22:28:41 -0300

    feat: add full responsive content

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const mobileMenu = document.getElementById("mobile-menu")
+const mobileNav = document.getElementById("mobile-nav")
+
+function toggleMenu(btn) {
+    mobileNav.classList.toggle("show")
+}
