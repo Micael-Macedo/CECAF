307a7857040737ea73ddf2166780876b4b0db765
Micael Macedo <micaelm2009@hotmail.com>
2024-03-01 07:11:52 -0300

    fix: header img size

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+$from = "";
+$to      = 'micael.barbosa@fieb.org.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: micaelm2009@hotmail.com' . "\r\n" .
+    'Reply-To: micaelm2009@hotmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+?>
