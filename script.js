415c235a681bcd930505c66d8b55751cc1bce7e3
Micael Macedo <micaelm2009@hotmail.com>
2024-03-03 02:37:24 -0300

    feat: add functional carrousel

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const mobileNav = document.getElementById("mobile-nav")
 function toggleMenu(btn) {
     mobileNav.classList.toggle("show")
 }
+var currentNewPos = 0
+const carousel = document.querySelector(".gallery-wrapper")
+const news = document.querySelectorAll(".new")
+const btnsNews = document.getElementById("btns-news")
+const passNews = setInterval(() => {
+    btnsNews.children[currentNewPos].classList.toggle("btn-selected")
+    currentNewPos++
+    if (currentNewPos >= btnsNews.children.length) {
+        currentNewPos = 0
+    }
+    let newE = news[0].getClientRects()
+    carousel.scrollTo(currentNewPos * newE[0].width,0 )
+    btnsNews.children[currentNewPos].classList.toggle("btn-selected")
+}, 5000);
